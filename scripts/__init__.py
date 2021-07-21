69fdc4946d5f7ca102e52c29945132af3f5791c4
Elias <eandualem@gmail.com>
2021-07-21 12:48:37 +0300

    feat: update notebook

---
+++ scripts/__init__.py
@@ -0,0 +1,13 @@
+"""
+A Python package for scripts
+"""
+from .df_helper import DfHelper
+import sys as _sys
+import os as _os
+
+__version__ = '0.01'
+__author__ = 'Elias Andualem'
+__email__ = 'eandualem@gmail.com'
+
+
+__all__ = [DfHelper]
